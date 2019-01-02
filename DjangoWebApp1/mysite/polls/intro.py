d386c4c1ce277024d6c55fba9b70cb06995ae24f
Martin Macicek <martin.macicek@airbank.cz>
2019-01-02 17:25:43 +0100

    Build on generic views

--- DjangoWebApp1/mysite/polls/intro.py
+++ DjangoWebApp1/mysite/polls/intro.py
@@ -3,3 +3,7 @@
 # This project is versioned using GIT version control
 
 # admin username = admin, password = shoWfourcapa85
+
+# Development server
+# Change into the outer mysite directory,
+# >> python manage.py runserver
