a72eaf9c79d7e2d785fb7bd4ca1b254eff6b89d6
Martin Macicek <martin.macicek@airbank.cz>
2019-01-15 17:57:27 +0100

    Add import and add rollback procedure

--- DjangoWebApp1/WebScraping/LoadISIR/RenameClientsFolders_CollCase.py
+++ DjangoWebApp1/WebScraping/LoadISIR/RenameClientsFolders_CollCase.py
@@ -1,55 +1,78 @@
 
 import os
-#import csv
+import csv
 from tqdm import tqdm # progress bar in console
 
 # all folders in which subfolders need to be modified
 folders_dict = {
-    "EprPodaneZaloby": "W:\\Vymahani\\LATE\\EPR\\PODANÉ ŽALOBY",
-    "EprNachystane": "W:\\Vymahani\\LATE\\EPR\\NACHYSTANÉ",
-    "EprDebety": "W:\\Vymahani\\LATE\\EPR\\DEBETY",
-    "ExePodane": "W:\\Vymahani\\LATE\\EXEKUCE\\PODANÉ",
-    "ExePripravene": "W:\\Vymahani\\LATE\\EXEKUCE\\PŘIPRAVENÉ",
-    "InsPripravene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIPRAVENÉ",
-    "InsPrihlasene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIHLÁŠENÉ",
-    "DED": "W:\\Vymahani\\LATE\\DĚDICKÉ",
-    "DEDICOVE": "W:\\Vymahani\\LATE\\DĚDICOVÉ",
+    # "EprPodaneZaloby": "W:\\Vymahani\\LATE\\EPR\\PODANÉ ŽALOBY", Error .)
+    # "EprNachystane": "W:\\Vymahani\\LATE\\EPR\\NACHYSTANÉ", Error .)
+    # "EprDebety": "W:\\Vymahani\\LATE\\EPR\\DEBETY", Error .)
+    # "ExePodane": "W:\\Vymahani\\LATE\\EXEKUCE\\PODANÉ", Error .)
+    # "ExePripravene": "W:\\Vymahani\\LATE\\EXEKUCE\\PŘIPRAVENÉ", Error .)
+    # "InsPripravene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIPRAVENÉ", Error .)
+    # "InsPrihlasene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIHLÁŠENÉ", Error .)
+    # "DED": "W:\\Vymahani\\LATE\\DĚDICKÉ", Error .)
+    # "DEDICOVE": "W:\\Vymahani\\LATE\\DĚDICOVÉ", Error .)
     "TEST_EPR": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EPR_PODANE",
-    "TEST_EXE": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EXE_PRIPRAVENE",
-    "csv_with_clients" : "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\clientCodesTranslator.csv"
+    "TEST_EXE": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EXE_PRIPRAVENE"
+
 }
+
 
 # generate this dictionary from a database in a following way CUID: ID_COLL_CASE
-clients_dict = {2: 3,
-                5:6,
-                8:9,
-                11:12,
-                14:15
-}
+path_to_csv = "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\cuidCCTranslator.csv"
+
+with open(path_to_csv) as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    clients_dict = {}
+
+    for row in reader:
+        clients_dict[row['CUID']] = row['COLL_CASE']
 
 
-path_to_csv = folders_dict["csv_with_clients"]
-path_to_folder = folders_dict["TEST_EPR"]
+# Deployment code
+
+for folder in folders_dict:
+    path_to_folder = folders_dict[folder]
+    print(path_to_folder)
+    # Get all files in folder
+    filenames = os.listdir(path_to_folder)
+
+    for client in tqdm(clients_dict):
+        cuid = client
+        id_coll_case = clients_dict[client]
+
+        for filename in filenames:  # loop through all files and subfolders in Folder
+            if os.path.isdir(
+                    os.path.join(os.path.abspath(path_to_folder), filename)):  # check whether the current object is a folder or not
+
+                if filename[filename.rfind(" ")+1:] == str(cuid) and filename.find("CC") == -1:  # check if the CUID in folder name equals client CUID and does not already contain label
+
+                    # DO NOT RUN ON PRODUCTION ONLY YOU REALLY WANT TO RUN IT
+                    os.rename(
+                        path_to_folder + "\\" + filename,
+                        path_to_folder + "\\" + filename + " CC" + str(id_coll_case))
+                    print("Found and renamed " + filename + " CC" + str(id_coll_case))
 
 
+# RollBack code - if anything goes wrong .)
 
-# with open(path_to_csv) as f:
-#     reader = csv.reader(f)
-#     clients_list = list(reader)
+for folder in folders_dict:
+    path_to_folder = folders_dict[folder]
+    print(path_to_folder)
+    # Get all files in folder
+    filenames = os.listdir(path_to_folder)
 
-# Get all files in folder
-filenames = os.listdir(path_to_folder)
-
-for client in tqdm(clients_dict):
-    cuid = client
-    id_coll_case = clients_dict[client]
-
-    for filename in filenames:  # loop through all the files and folders
+    for filename in tqdm(filenames):  # loop through all the files and folders
         if os.path.isdir(
-                os.path.join(os.path.abspath(path_to_folder), filename)):  # check whether the current object is a folder or not
+                os.path.join(os.path.abspath(path_to_folder),
+                             filename)):  # check whether the current object is a folder or not
 
-            if filename.find(str(cuid)) != -1 and filename.find("CC") == -1:  # check if the folder name contains client CUID and does not already contain label
-                print("Found and renamed " + filename)
+            if filename.find("CC") > 0:  # check if there is already a labed
+
                 os.rename(
                     path_to_folder + "\\" + filename,
-                    path_to_folder + "\\" + filename + " CC" + str(id_coll_case))
+                    path_to_folder + "\\" + filename[:filename.rfind("CC") - 1])
+
+                print("Found and rollbacked " + filename)
