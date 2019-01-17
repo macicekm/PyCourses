0a271a47c9a4e2d937535f0af1dfceee402e2b68
Martin Macicek <martin.macicek@airbank.cz>
2019-01-17 13:47:26 +0100

    Add missing folder log

--- DjangoWebApp1/WebScraping/LoadISIR/RenameClientsFolders_CollCase.py
+++ DjangoWebApp1/WebScraping/LoadISIR/RenameClientsFolders_CollCase.py
@@ -8,17 +8,17 @@ dtime_start = datetime.datetime.now()
 
 # all folders in which subfolders need to be modified
 folders_dict = {
-    "EprPodaneZaloby": "W:\\Vymahani\\LATE\\EPR\\PODANÉ ŽALOBY",
-    "EprNachystane": "W:\\Vymahani\\LATE\\EPR\\NACHYSTANÉ",
-    "EprDebety": "W:\\Vymahani\\LATE\\EPR\\DEBETY",
-    "ExePodane": "W:\\Vymahani\\LATE\\EXEKUCE\\PODANÉ",
-    "ExePripravene": "W:\\Vymahani\\LATE\\EXEKUCE\\PŘIPRAVENÉ",
-    "InsPripravene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIPRAVENÉ",
-    "InsPrihlasene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIHLÁŠENÉ",
-    "DED": "W:\\Vymahani\\LATE\\DĚDICKÉ",
-    "DEDICOVE": "W:\\Vymahani\\LATE\\DĚDICOVÉ",
-    # "TEST_EPR": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EPR_PODANE",
-    # "TEST_EXE": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EXE_PRIPRAVENE"
+    # "EprPodaneZaloby": "W:\\Vymahani\\LATE\\EPR\\PODANÉ ŽALOBY",
+    # "EprNachystane": "W:\\Vymahani\\LATE\\EPR\\NACHYSTANÉ",
+    # "EprDebety": "W:\\Vymahani\\LATE\\EPR\\DEBETY",
+    # "ExePodane": "W:\\Vymahani\\LATE\\EXEKUCE\\PODANÉ",
+    # "ExePripravene": "W:\\Vymahani\\LATE\\EXEKUCE\\PŘIPRAVENÉ",
+    # "InsPripravene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIPRAVENÉ",
+    # "InsPrihlasene": "W:\\Vymahani\\LATE\\Insolvence\\PŘIHLÁŠENÉ",
+    # "DED": "W:\\Vymahani\\LATE\\DĚDICKÉ",
+    # "DEDICOVE": "W:\\Vymahani\\LATE\\DĚDICOVÉ",
+     "TEST_EPR": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EPR_PODANE",
+     "TEST_EXE": "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\EXE_PRIPRAVENE"
 }
 
 # generate this dictionary from a database in a following way CUID: ID_COLL_CASE
@@ -36,6 +36,15 @@ with open(path_to_csv) as csvfile:
         clients_dict[row['FOLDER_NAME']] = row['COLL_CASE']
 
 
+
+
+
+##############################################################################################################################################################
+# Deployment code
+
+print('--X--X--X--X--')
+print('Renaming Folders')
+
 # For log
 path_to_log = "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\folderRenameLog.csv"
 log_file = open(path_to_log, 'w')
@@ -43,15 +52,9 @@ log_file = open(path_to_log, 'w')
 log_file.write("DTIME_INSERTED;ACTION;DIRECTORY;FOLDER")  # log header
 log_file.write('\n')  # new line
 
-
-
-# Deployment code
-
 for folder in folders_dict:
     path_to_folder = folders_dict[folder]
     print(path_to_folder)
-    # Get all files in folder
-    filenames = os.listdir(path_to_folder)
 
     for client in tqdm(clients_dict):
         name_cuid = client
@@ -63,7 +66,7 @@ for folder in folders_dict:
             if filename.find("CC") == -1:  # check if the CUID in folder name equals client CUID and does not already contain label
 
                 # DO NOT RUN ON PRODUCTION ONLY IF YOU REALLY WANT TO RUN IT - BEGIN
-                # os.rename(filename, filename + " CC" + str(id_coll_case)) ERRRRRROR
+                # os.rename(filename, filename + " CC" + str(id_coll_case))
                 # DO NOT RUN ON PRODUCTION ONLY IF YOU REALLY WANT TO RUN IT - END
 
                 log_file.write(str(datetime.datetime.now()) + ";Found and renamed;" + folder + ";" + filename + " CC" + str(id_coll_case))
@@ -77,9 +80,54 @@ for folder in folders_dict:
             log_file.write('\n')  # new line
 
 
+##############################################################################################################################################################
+# Write log of all client folders, that are left and have not been renamed - name does not include CC
 
+print('--X--X--X--X--')
+print('Missing CC log')
+
+# For log
+path_to_log = "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\folderMissedRenameLog.csv"
+log_file = open(path_to_log, 'w')
+
+log_file.write("DTIME_INSERTED;ACTION;DIRECTORY;FOLDER")  # log header
+log_file.write('\n')  # new line
+
+for folder in folders_dict:
+    path_to_folder = folders_dict[folder]
+    print(path_to_folder)
+    # Get all files in folder
+    filenames = os.listdir(path_to_folder)
+
+    for filename in tqdm(filenames):  # loop through all the files and folders
+        if os.path.isdir(
+                os.path.join(os.path.abspath(path_to_folder),
+                             filename)):  # check whether the current object is a folder or not
+
+            if filename.find("CC") == -1:  # check whether label is missing
+
+                try:
+                    log_file.write(str(datetime.datetime.now()) + ";CC Label Missing;" + folder + ";" + path_to_folder + "\\" + filename)
+                except:
+                    log_file.write(str(datetime.datetime.now()) + ";CC Exception occurred;" + folder + ";" + path_to_folder)
+                finally:
+                    log_file.write('\n')  # new line
+
+
+##############################################################################################################################################################
 # RollBack code - if anything goes wrong .)
 if False:
+
+    print('--X--X--X--X--')
+    print('Rolling back')
+
+    # For log
+    path_to_log = "D:\\Users\\mmacicek1695ab\\Desktop\\Work\\Tasks\\GITProjects\\test\\folderRollbackLog.csv"
+    log_file = open(path_to_log, 'w')
+
+    log_file.write("DTIME_INSERTED;ACTION;DIRECTORY;FOLDER")  # log header
+    log_file.write('\n')  # new line
+
     for folder in folders_dict:
         path_to_folder = folders_dict[folder]
         print(path_to_folder)
@@ -96,11 +144,12 @@ if False:
                     # DO NOT RUN ON PRODUCTION ONLY IF YOU REALLY WANT TO RUN IT - BEGIN
                     # os.rename(
                     #     path_to_folder + "\\" + filename,
-                    #     path_to_folder + "\\" + filename[:filename.rfind("CC") - 1]) ERRRRRROR
+                    #     path_to_folder + "\\" + filename[:filename.rfind("CC") - 1])
                     # DO NOT RUN ON PRODUCTION ONLY IF YOU REALLY WANT TO RUN IT - END
 
                     log_file.write(str(datetime.datetime.now()) + ";Found and rollbacked;" + folder + ";" + path_to_folder + "\\" + filename)
                     log_file.write('\n')  # new line
 
 
-print("Finished, time elapsed = " + str(round((datetime.datetime.now() -dtime_start).total_seconds()/60,2)) + " minutes")
+##############################################################################################################################################################
+print("Finished, time elapsed = " + str(round((datetime.datetime.now() -dtime_start).total_seconds()/60, 2)) + " minutes")
